4202c77a02bb25bba89911b4174032b3495eb3c8
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-01-01 20:21:29 +0800

    initial commit

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+import Card from '../UI/Card'
+function Expenses(props) {
+
+  return (
+    <Card className="expenses">
+
+<ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
+    </Card>
+  )
+
+}
+export default Expenses;
